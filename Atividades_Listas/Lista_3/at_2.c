b2bfb231d29f799e01ff4797b1a8317918daf7ce
mhrs-cin <mhrs@cin.ufpe.br>
2026-03-19 13:09:12 -0300

    At_2.fim

--- Atividades_Listas/Lista_3/at_2.c
+++ Atividades_Listas/Lista_3/at_2.c
@@ -4,33 +4,34 @@
 int main(void){
 
     char Start_time[] = "07:03"; // tempo inicial
-    int days = 0; // variável desnecessária para contar dias
+    int days = 0; // vari?vel desnecess?ria para contar dias
     int Current_client = 1, Client_amount = 0, MULT = 0, Greedy_clients = 0, Bought_cans = 0;
     
     scanf("%d %d %d\n", &Client_amount, &MULT, &Greedy_clients);
 
-    char Greedy_array[Greedy_clients][5]; int Array_greedy[Greedy_clients];
-
+    char Greedy_array[(Greedy_clients*5)+1]; int Array_greedy[Greedy_clients];
     
     for(int i = 0; i<Greedy_clients; i++){
-        for(int j = 0; j<6; j++){
+        for(int j = 0+(i*5); j<6+(i*5); j++){
             char tempchar;
             int tempint;
-            if(j<5){scanf("%c", &tempchar); Greedy_array[i][j] = tempchar;}else{scanf(" %d\n", &tempint); Array_greedy[i] = tempint;}
+            if(j<5+(i*5)){scanf("%c", &tempchar); Greedy_array[j] = tempchar;}else{scanf(" %d\n", &tempint); Array_greedy[i] = tempint;}
         }
-    }   
+    }
     
-
     while(Current_client<=Client_amount){
 
         int Equal_num = -1;
 
         for(int i = 0; i<Greedy_clients; i++){
             int Is_equal = 1;
-            for(int j = 0; j<5; j++){
-                if(Greedy_array[i][j]!=Start_time[j]){
+            int cont = 0;
+
+            for(int j = 0+(i*5); j<5+(i*5); j++){
+                if(Greedy_array[j] != Start_time[cont]){
                     Is_equal = 0;
                 }
+                cont++;
             }
             if(Is_equal==1){
                 Equal_num = i;
@@ -58,7 +59,7 @@ int main(void){
 
         Start_time[4] = Start_time[4]+3;
 
-        while(Start_time[4]>'9' || Start_time[4]<'0'){ //sequência de while loops que corrigem o horário;
+        while(Start_time[4]>'9' || Start_time[4]<'0'){ //sequ?ncia de while loops que corrigem o hor?rio;
             Start_time[4] = Start_time[4]-10;
             Start_time[3] = Start_time[3]+1;
         }
@@ -91,7 +92,9 @@ int main(void){
         Current_client++;
     }
 
-    if(Bought_cans<50){
+    if(Bought_cans==49){
+        printf("Ainda nao foram vendidas latas suficientes. Falta %d lata.", 50-Bought_cans);
+    }else if(Bought_cans<50){
         printf("Ainda nao foram vendidas latas suficientes. Faltam %d latas.", 50-Bought_cans);
     }
 
